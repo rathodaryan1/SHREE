57c056f227e58f80731fe5e6676dbbbebac45574
Rathod Aryan <aaryan.b.rathod99@gmail.com>
2025-05-23 13:09:49 +0530

    Initial commit

--- server/vite.ts
+++ server/vite.ts
@@ -26,20 +26,19 @@ export async function setupVite(app: Express, server: Server) {
     allowedHosts: true as true,
   };
 
-const vite = await createViteServer({
-  ...viteConfig,
-  configFile: false,
-  customLogger: {
-    ...viteLogger,
-    error: (msg, options) => {
-      viteLogger.error(msg, options);
-      process.exit(1);
+  const vite = await createViteServer({
+    ...viteConfig,
+    configFile: false,
+    customLogger: {
+      ...viteLogger,
+      error: (msg, options) => {
+        viteLogger.error(msg, options);
+        process.exit(1);
+      },
     },
-  },
- server:serverOptions,
-  appType: "custom",
-});
-
+    server: serverOptions,
+    appType: "custom",
+  });
 
   app.use(vite.middlewares);
   app.use("*", async (req, res, next) => {
